5cfed0d4d28f222784cc0f8f6ee8f728cb0546cd
leminhthai0303 <leminhthai0303@gmail.com>
2023-01-22 12:10:24 +0700

    Fixing the printing message

--- python_blackjack.py
+++ python_blackjack.py
@@ -1,4 +1,5 @@
 import random
+import cowsay
 
 suits = ['Diamond','Club','Spade','Heart']
 values = ['Ace', 2,3,4,5,6,7,8,9,10,'J','Q','K']
@@ -35,9 +36,13 @@ def is_blackjack(hand):
     if 'Ace' in first_card:
         if 'J' in second_card or 'Q' in second_card or 'K' in second_card or int(value2) == 10:
             return True
+        else:
+            return False
     elif 'Ace' in second_card:
         if 'J' in first_card or 'Q' in first_card or 'K' in first_card or int(value1) == 10:
             return True
+        else:
+            return False
     elif 'Ace' in first_card and 'Ace' in second_card:
         return True
     else:
@@ -121,21 +126,21 @@ def main():
     if player != True and dealer != True:
         if player <= 21 and dealer <= 21:
             if player > dealer:
-                print("You WIN!!!")
+                cowsay.cow("You WIN!!!")
             elif player == dealer:
-                print("Draw")
+                cowsay.stimpy("Draw")
             else:
-                print("You LOSE!!!")
+                cowsay.dragon("You LOSE!!!")
         elif player > 21 and dealer <= 21:
-            print("You LOSE!!!")
+            cowsay.dragon("You LOSE!!!")
         elif player <= 21 and dealer > 21:
-            print("You win")
+            cowsay.cow("You win")
         elif player > 21 and dealer > 21:
-            print("Draw")
+            cowsay.stimpy("Draw")
     elif player != True and dealer == True:
-        print('THE DEALER GOT BLACKJACK!!! YOU LOSE!!!')
+        cowsay.daemon('THE DEALER GOT BLACKJACK!!! YOU LOSE!!!')
     elif player == True and dealer != True:
-        print('BLACKJACK!!! YOU WIN!!!')
+        cowsay.pig('BLACKJACK!!! YOU WIN!!!')
 
 if __name__ == '__main__':
     main()
